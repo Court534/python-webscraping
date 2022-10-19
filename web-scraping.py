abb0a546efc75616d2be90e5855f76638231231b
Courtney Stow <courney-stow@hotmail.co.uk>
2022-10-19 11:36:42 +0100

    Scraped all the information down from .tombstone container, .short-desc, .temp and .img and put them in a table using pandas

--- web-scraping.py
+++ web-scraping.py
@@ -1,4 +1,5 @@
 import requests
+import pandas as pd
 
 from bs4 import BeautifulSoup
 page = requests.get("https://forecast.weather.gov/MapClick.php?lat=37.7772&lon=-122.4168")
@@ -11,6 +12,7 @@ forecast_items = seven_day.find_all(class_="tombstone-container")
 tonight = forecast_items[1]
 tomorrow = forecast_items[3]
 friday = forecast_items[5]
+
 
 # period if finding the first class with the name period-name and returning all the text within it using .get_text()
 # short_desc if finding the first class with the name short-desc and returning all the text within it using .get_text()
@@ -47,3 +49,23 @@ print(temp)
 img = friday.find("img")
 desc = img['title']
 print(desc)
+print('\n')
+
+period_tags = seven_day.select(".tombstone-container .period-name")
+periods = [pt.get_text() for pt in period_tags]
+
+short_descs = [sd.get_text() for sd in seven_day.select(".tombstone-container .short-desc")]
+temps = [t.get_text() for t in seven_day.select(".tombstone-container .temp")]
+descs = [d["title"] for d in seven_day.select(".tombstone-container img")]
+print(short_descs)
+print(temps)
+print(descs)
+print('\n')
+
+weather = pd.DataFrame({
+    "period": periods,
+    "short_desc": short_descs,
+    "temp": temps,
+    "desc": descs
+})
+print(weather)
